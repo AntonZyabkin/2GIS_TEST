9d56f3b8cadf9cda8a47f9213729d7bb64953b2e
Anatan <zyabkinantonv@gmail.com>
2023-05-16 09:27:31 +0300

    init commit

---
+++ 2GIS_TEST/_GIS_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  _GIS_TESTApp.swift
+//  2GIS_TEST
+//
+//  Created by Anton Zyabkin on 16.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct _GIS_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
